9bedddf61229f135bc977e15775112341d3cf8c5
JhoJho19 <eugb46@gmail.com>
2024-04-10 19:25:46 +0300

    10.04.2024 19.25

---
+++ BlackJack/Assets/Scripts/StartButBeh.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButBeh : MonoBehaviour
+{
+    [SerializeField] Image buttonImage;
+    [SerializeField] Sprite buttonGreen;
+    [SerializeField] Sprite buttonGrey;
+
+    [SerializeField] Sprite pikeOne;
+    [SerializeField] Sprite pikeTwo;
+    [SerializeField] Sprite pikeThree;
+    [SerializeField] Sprite pikeFour;
+    [SerializeField] Sprite pikeFive;
+    [SerializeField] Sprite pikeSix;
+    [SerializeField] Sprite pikeSeven;
+    [SerializeField] Sprite pikeEight;
+    [SerializeField] Sprite pikeNine;
+    [SerializeField] Sprite pikeTen;
+    [SerializeField] Sprite pikeJack;
+
+
+
+
+
+
+}
